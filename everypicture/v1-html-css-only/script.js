078321e7ab35edc45b8bc0adfbb488d9c534d52a
apvincent-jpg <apvincent@ucdavis.edu>
2026-02-23 17:48:59 -0800

    reload

--- everypicture/v1-html-css-only/script.js
+++ everypicture/v1-html-css-only/script.js
@@ -1,15 +1,27 @@
 //make image fully load before starting anything 
 window.addEventListener('load', function () {
 
-	
 	const posts = document.querySelectorAll('section');
+	const original = this.document.querySelector('.originalImg'); 
 	let postTops = [];
-	let pageTop;
 	let counter = 1;
 	let prevCounter = 1;
-	let doneResizing;
 
-	
+	//see where each section will start
+	function resetPagePosition() {
+        postTops = [];
+        for (let i = 0; i < posts.length; i++) {
+            postTops.push(posts[i].getBoundingClientRect().top + window.pageYOffset);
+        }
+
+        // set counter based on scroll position
+        let pagePosition = window.pageYOffset + 300;
+        counter = 0;
+        for (let i = 0; i < postTops.length; i++) {
+            if (pagePosition > postTops[i]) counter++;
+        }
+    }
+
 	resetPagePosition();
 
 	window.addEventListener('scroll', function () {
@@ -18,40 +30,55 @@ window.addEventListener('load', function () {
 		// scroll down
 		if (pageTop > postTops[counter]) {
 			counter++;
-			console.log(`scrolling down ${counter}`);
 		}
 		// scrolling up
 		else if (counter > 1 && pageTop < postTops[counter - 1]) {
 			counter--;
-			console.log(`scrolling up ${counter}`);
 		}
 
 		// section change
 		if (counter != prevCounter) {
-			document.querySelector('figure img').className = 'sect' + counter;
-			prevCounter = counter;
-		}
+			
+			// hide overlays
+			document.querySelector('.pockets').classList.remove('show');
+            document.querySelector('.jugs').classList.remove('show');
+            document.querySelector('.sidepulls').classList.remove('show');
+            document.querySelector('.footholds').classList.remove('show');
+            document.querySelector('.crimps').classList.remove('show');
+			
 
+			//showing each overlay
+			  if (counter == 2) {
+                document.querySelector('.pockets').classList.add('show');
+            } 
+            else if (counter == 3) {
+                document.querySelector('.jugs').classList.add('show');
+            } 
+            else if (counter == 4) {
+                document.querySelector('.sidepulls').classList.add('show');
+            } 
+            else if (counter == 5) {
+                document.querySelector('.footholds').classList.add('show');
+            } 
+            else if (counter == 6) {
+                document.querySelector('.crimps').classList.add('show');
+            }
+
+			//fade in image initially
+			if (counter > 1) {
+		original.style.opacity = 0.5;  // fade original when first overlay appears
+			} else {
+    	original.style.opacity = 1;    // fully visible at the very top
+			}
+		
+
+            prevCounter = counter;
+        }
 	}); 
 
+	
+	
+	
 
-	window.addEventListener('resize', function () {
-
-		clearTimeout(doneResizing);
-		doneResizing = setTimeout(function () {
-			resetPagePosition();
-		}, 500);
-	});
-
-
-	function resetPagePosition() {
-		postTops = [];
-		posts.forEach(function (post) {
-			postTops.push(Math.floor(post.getBoundingClientRect().top) + window.pageYOffset);
-		});
-
-		const pagePosition = window.pageYOffset + 300;
-		counter = 0;
-		postTops.forEach(function (post) { if (pagePosition > post) { counter++; } });
-	}
+	
 }); 
